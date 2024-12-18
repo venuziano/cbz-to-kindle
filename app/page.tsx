707236f6be6b30d0d2a5ffb36a8c4c41af473807
Rafael <raf4.rrs@gmail.com>
2024-12-18 00:03:54 -0300

    ADDED: Testing ga

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
+export const dynamic = 'force-dynamic';
+
 import React, { useCallback, useEffect, useState } from 'react';
 import { PDFDocument } from 'pdf-lib';
 import JSZip from 'jszip';
