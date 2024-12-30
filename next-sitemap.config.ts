a7a7a1cc02ca6b45e8091821d4c0b40b5d02bd01
Rafael <raf4.rrs@gmail.com>
2024-12-30 20:26:24 -0300

    ADDED: Adding search engine validations

---
+++ next-sitemap.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.cbz-to-pdf.com.br/', // Your production URL
+  changefreq: 'daily',               // default change frequency
+  priority: 0.7,                     // default priority
+  // Other options
+};
