67a0a43d4b6636472db1587ed4dca2d76628f67e
Rafael <raf4.rrs@gmail.com>
2024-12-31 14:38:45 -0300

    FIXED: Move get country api to the ssr side

--- app/api/getCountry/route.ts
+++ app/api/getCountry/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
     const ipResponse: Response = await fetch('https://api.ipify.org?format=json');
     const ipData = await ipResponse.json();
     const userIP: string = ipData.ip;
-
+    console.log('userIP', userIP)
     const countryResponse: Response = await fetch(`https://ipapi.co/${userIP}/country/`);
     const country: string = await countryResponse.text();
 
