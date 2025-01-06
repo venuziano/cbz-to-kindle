d35f569d514074a17a97db73fbf469e6d1a7f0a4
Rafael <raf4.rrs@gmail.com>
2025-01-06 19:11:03 -0300

    IMPROVED: Adding sentry sourcemap

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
-export default withSentryConfig(nextConfig, {
+export default withSentryConfig(withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
@@ -18,12 +18,11 @@ export default withSentryConfig(nextConfig, {
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-  tunnelRoute: "/api/monitoring", // Use your SSR route here,
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  // tunnelRoute: "/monitoring",
+  tunnelRoute: "/api/monitoring", // Use your SSR route here,
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
@@ -35,5 +34,11 @@ export default withSentryConfig(nextConfig, {
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true
-});
+  automaticVercelMonitors: true,
+
+
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+}));
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,10 +5,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
-
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
